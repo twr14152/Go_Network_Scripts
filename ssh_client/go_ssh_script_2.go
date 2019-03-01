2de9d75ae398f43463c0de64273e180b21134da0
Todd Riemenschneider <twr14152@gmail.com>
2019-03-01 12:58:28 -0500

    Update go_ssh_script_2.go

--- ssh_client/go_ssh_script_2.go
+++ ssh_client/go_ssh_script_2.go
@@ -1,6 +1,7 @@
-// This script will run multiple show commands on a device. 
-// The issue is that it needs to reconnect with each command it iterates through.
-// Using Map instead of slice or array. May come in handy down the road.
+// This script is used to run show commands on a cisco devnet device
+// I used a map to create the command set that will be run on the device
+// The for loop allows us to run multiple commands on the device.
+// (c) 2019 Todd Riemenschneider
 
 package main
 
@@ -12,32 +13,34 @@ import (
 )
 
 func main() {
-
-	commands := map[string]string{"a":"show ip arp", "b":"show version"}
-
+        // Used a map to build my command set
+	commands := map[string]string{"a":"show ip arp", "b":"show version", "c":"show ip route summ"}
+        // SSH Creds to log into remote device
 	config := &ssh.ClientConfig{
 		User:            "root",
 		Auth:            []ssh.AuthMethod{ssh.Password("D_Vay!_10&")},
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
-
+        // Used for loop to run through command set, "_" throws away the key data while v represents the actual command
 	for _, v := range commands {
-            // Issues new connection with each command....Not sure what to think about that.....
+               //This is who the script will be logging into
                client, err := ssh.Dial("tcp", "ios-xe-mgmt.cisco.com:8181", config)
                if err != nil {
                         log.Fatal("Failed to dial: ", err)
                }
-            // Each ClientConn can support multiple interactive sessions,
-            // represented by a Session.
+                //This initiates the connection to the remote device
                 session, err := client.NewSession()
                 if err != nil {
                         log.Fatal("Failed to create session: ", err)
                 }
                 defer session.Close()
+                // Defines a variable to capture the output of the commands being run
                 var cmd_output bytes.Buffer
                 session.Stdout = &cmd_output
                 fmt.Printf("**** %s *** ", v)
+                //This is where the commands actually being sent to the remote device
                 session.Run(v)
+                // Prints output to screen
                 fmt.Println(cmd_output.String())
 	fmt.Printf("###########\n")
 }
