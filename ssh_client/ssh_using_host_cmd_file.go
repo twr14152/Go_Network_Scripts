5836dec19d0278f8793d6b7086fe0f873d4448a2
Todd Riemenschneider <twr14152@gmail.com>
2019-10-29 20:34:19 -0400

    Create ssh_using_host_cmd_file.go

---
+++ ssh_client/ssh_using_host_cmd_file.go
@@ -0,0 +1,144 @@
+// This ssh script will use host_file.txt to determine who to 
+// login to, and the cmd_file1.txt to determine which commands
+// to run.
+// (c) 2019 Todd Riemenschneider
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/crypto/ssh"
+	"log"
+	"os"
+)
+
+var hostList []string
+
+func loginHosts() {
+	hf, _ := os.Open("host_file.txt")
+	scanner := bufio.NewScanner(hf)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		hostList = append(hostList, scanner.Text())
+		// scaffolding for troubleshooting
+		//       fmt.Println(hostList)
+	}
+}
+func main() {
+	loginHosts()
+	//scaffolding for troubleshooting
+	//for _, host := range hostList {
+	//		fmt.Println(host)
+	for _, host := range hostList {
+		user := "developer"
+		pass := "C1sco12345"
+		targethost := host
+
+		config := &ssh.ClientConfig{
+			User: user,
+			Auth: []ssh.AuthMethod{
+				ssh.Password(pass),
+			},
+			HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		}
+		conn, err := ssh.Dial("tcp", targethost, config)
+		//Scaffolding for troubleshooting
+		//fmt.Println(conn)
+		if err != nil {
+			log.Fatal("Failed to dial: ", err)
+		}
+		sess, err := conn.NewSession()
+		if err != nil {
+			log.Fatal("Failed to create session: ", err)
+		}
+		stdin, _ := sess.StdinPipe()
+		sess.Stdout = os.Stdout
+		sess.Stderr = os.Stderr
+		sess.Shell()
+		fh, _ := os.Open("cmd_file1.txt")
+		scanner := bufio.NewScanner(fh)
+		scanner.Split(bufio.ScanLines)
+		var lines []string
+		for scanner.Scan() {
+			lines = append(lines, scanner.Text())
+		}
+		fh.Close()
+		for _, line := range lines {
+			fmt.Fprintf(stdin, "%s\n", line)
+		}
+		sess.Wait()
+		sess.Close()
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
