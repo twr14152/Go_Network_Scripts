34caceeb7286c50d683ef5d1e74ca9e060ecf027
Todd Riemenschneider <twr14152@gmail.com>
2019-11-04 17:32:46 -0500

    Update httpPost_v2.go

--- http_client/httpPost_v2.go
+++ http_client/httpPost_v2.go
@@ -23,11 +23,11 @@ func main() {
 	certInfo := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
-	//Just some code to see what the configs look like in byte code and str fmt
-	//fmt.Println("config in bytes then in str fmt:\n ", config,"\n", string(config))
 	//Apply the tls cert bypass to the client variable
 	client := &http.Client{Transport: certInfo}
 	for _, host := range hostList {
+		//Just some code to see what the configs look like in byte code and str fmt
+		fmt.Println("The config being loaded:\n", string(config))
 		// creating variable URL make the http.NewRequest statement easier to read
 		url := "https://"+host+"/restconf/data/ietf-interfaces:interfaces"
 		// The POST method is used to apply the (config) variable
