3abe3c69212e95bb1201f8ba06fa0ae0287581d8
Todd Riemenschneider <twr14152@gmail.com>
2019-11-20 07:16:23 -0500

    Create methodsTest.go

---
+++ misc_folder/methodsTest.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type Switch struct {
+	Name        string
+	Interface   string
+	IntDescr    string
+	Ip          string
+	RoutingProt string
+	Network     string
+}
+// Testing method concept
+func (sw *Switch) Details() {
+	fmt.Println("hostname", sw.Name)
+	fmt.Println(" interface", sw.Interface)
+	fmt.Println(sw.IntDescr)
+	fmt.Println("", sw.Ip, "255.255.255.255")
+	fmt.Println("router", sw.RoutingProt)
+	fmt.Println(" network", sw.Network, "255.255.255.0")
+}
+
+func main() {
+	core1 := Switch{Name: "CORE_SW1", Interface: "loopback0", IntDescr: " management interface CORE_SW1", Ip: "65.90.100.1", RoutingProt: "bgp 65001", Network: "10.0.0.0"}
+	core2 := Switch{Name: "CORE_SW2", Interface: "loopback0", IntDescr: " management interface CORE_SW2", Ip: "65.90.100.2", RoutingProt: "bgp 65001", Network: "10.0.0.0"}
+	core1.Details()
+	fmt.Println("+++++++++++++++++++")
+	core2.Details()
+}
