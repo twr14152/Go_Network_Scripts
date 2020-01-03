dc946856ea4fef0c5a2f5fa93ef739ae89866e26
Todd Riemenschneider <twr14152@gmail.com>
2020-01-03 10:48:26 -0500

    Update methodsTest.go

--- misc_folder/methodsTest.go
+++ misc_folder/methodsTest.go
@@ -10,6 +10,7 @@ type Switch struct {
 	RoutingProt string
 	Network     string
 }
+
 // Testing Method
 func (sw *Switch) confInfo() {
 	fmt.Println("hostname", sw.Name)
@@ -23,7 +24,12 @@ func (sw *Switch) confInfo() {
 func main() {
 	core1 := Switch{Name: "CORE_SW1", Interface: "loopback0", IntDescr: " management interface CORE_SW1", Ip: "65.90.100.1", RoutingProt: "bgp 65001", Network: "10.0.0.0"}
 	core2 := Switch{Name: "CORE_SW2", Interface: "loopback0", IntDescr: " management interface CORE_SW2", Ip: "65.90.100.2", RoutingProt: "bgp 65001", Network: "10.0.0.0"}
+	// Using positional parameters for var assignment in Switch struct
+	core3 := Switch{"CORE_SW3", "Loopback1", "Test Loopback on CORE_SW3", "10.10.10.2", "ospf 100", "10.10.10.0"}
+
 	core1.confInfo()
 	fmt.Println("+++++++++++++++++++")
 	core2.confInfo()
+	fmt.Println("+++++++++++++++++++")
+	core3.confInfo()
 }
