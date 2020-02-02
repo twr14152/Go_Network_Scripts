5f3ff3fcdc61d586d37e785d7900d3b03ea0839a
Todd Riemenschneider <twr14152@gmail.com>
2020-02-02 15:58:28 -0500

    Update show_cmds.go

--- goeapi_stuff/show_cmds.go
+++ goeapi_stuff/show_cmds.go
@@ -8,22 +8,21 @@ import (
 )
 
 func main() {
-	// This will create the list of the commands that you will run
+	// This will run the commands after your program
 	var s, sep string
 	for i := 1; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
 		sep = " "
 	}
 	cmds := strings.Split(s, ",")
-	fmt.Printf("%T ", cmds)
 	fmt.Println(cmds)
 	for _, host := range HostList {
 		node, err := goeapi.ConnectTo(host)
+		if err != nil {
+			fmt.Println(err)
+		}
 		fmt.Println("#############################")
 		fmt.Printf("Connected to %v\n", host)
-		if err != nil {
-			panic(err)
-		}
 		fmt.Println(node.Enable(cmds))
 		fmt.Println("############################")
 	}
