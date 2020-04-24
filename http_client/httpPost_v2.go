f9456591c401a5a39870d1a60d840ca9129ff779
Todd Riemenschneider <twr14152@gmail.com>
2020-04-24 15:28:23 -0400

    Update httpPost_v2.go

--- http_client/httpPost_v2.go
+++ http_client/httpPost_v2.go
@@ -23,29 +23,22 @@ func main() {
 	certInfo := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
-	//Apply the tls cert bypass to the client variable
 	client := &http.Client{Transport: certInfo}
 	for _, host := range hostList {
-		//Just some code to see what the configs look like in byte code and str fmt
 		fmt.Println("The config being loaded:\n", /*config, "\n",*/ string(config))
-		// creating variable URL make the http.NewRequest statement easier to read
 		url := "https://"+host+"/restconf/data/ietf-interfaces:interfaces"
-		// The POST method is used to apply the (config) variable
+
 		req, _ := http.NewRequest("POST", url, bytes.NewBuffer(config))
-		// Authentication Parameters
 		req.SetBasicAuth("developer", "C1sco12345")
-		// Header additions are required for the POST to work with restconf
 		req.Header.Add("Content-Type", "application/yang-data+json")
 		req.Header.Add("Accept", "application/yang-data+json")
-		// client.Do actually kicks off the process by sending http request 
 		res, _ := client.Do(req)
 		fmt.Println("Connecting to host: ", host)
-        	// client must close response body when finished
+     
 		defer res.Body.Close()
 		body, _ := ioutil.ReadAll(res.Body)
         	// This will print either created (success 201) or conflict (failure 409)  
 		fmt.Println("Device Response:\n ",(res))
-		// Print the body of the response as a string
 		fmt.Println("This is the output of body: ", string(body))
 	}	
 }
