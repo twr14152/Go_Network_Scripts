d8b7616b0b660a50ce704a588bdfcdfffda6a81d
Todd Riemenschneider <twr14152@gmail.com>
2020-04-24 20:15:06 -0400

    Update httpGet_v1.go

--- http_client/httpGet_v1.go
+++ http_client/httpGet_v1.go
@@ -1,15 +1,14 @@
-// This script will use https to pull router info 
+// This script will use https to pull router info
 // https://host:port/restconf/data/etc:etc will determine what info is gathered
 // (c) 2019 Todd Riemenschneider
-
 
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"io/ioutil"
 	"crypto/tls"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 func main() {
@@ -17,16 +16,17 @@ func main() {
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 	client := &http.Client{Transport: certInfo}
-	url := "https://ios-xe-mgmt.cisco.com:9443/restconf//data/Cisco-IOS-XE-native:native?content=config&depth=65535"
+	url := "https://ios-xe-mgmt-latest.cisco.com:9443/restconf/data/Cisco-IOS-XE-native:native?content=config&depth=65535"
 
 	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("Content-Type", "application/yang-data+json")
+	req.Header.Add("Accept", "application/yang-data+json")
 	req.SetBasicAuth("developer", "C1sco12345")
-	res, _ := client.Do(req)
 
+	res, _ := client.Do(req)
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
 
 	fmt.Println(res)
 	fmt.Println(string(body))
-
 }
