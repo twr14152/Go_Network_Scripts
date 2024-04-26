3f8f6231642e46b1f3c83bb0c0390f75804d83e7
Todd Riemenschneider <twr14152@gmail.com>
2024-04-25 21:39:03 -0400

    Update show_cmds.go

--- arista/goeapi/show_cmds.go
+++ arista/goeapi/show_cmds.go
@@ -1,31 +1,30 @@
 package main
 
 import (
-	"fmt"
-	"github.com/aristanetworks/goeapi"
-	"os"
-	"strings"
+        "fmt"
+        "github.com/aristanetworks/goeapi"
+        "os"
+        "strings"
 )
 
 var HostList = []string{"ceos1", "ceos2", "ceos3", "ceos4", "ceos5", "ceos6"}
 
 func main() {
-	// This will run the commands listed after the filename
-	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
-		sep = " "
-	}
-	cmds := strings.Split(s, ",")
-	fmt.Println(cmds)
-	for _, host := range HostList {
-		node, err := goeapi.ConnectTo(host)
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println("#############################")
-		fmt.Printf("Connected to %v\n", host)
-		fmt.Println(node.Enable(cmds))
-		fmt.Println("############################")
-	}
+        // This will run the commands listed after the filename
+        var s, sep string
+        for i := 1; i < len(os.Args); i++ {
+                s += sep + os.Args[i]
+                sep = " "
+        }
+        cmds := strings.Split(s, ",")
+        fmt.Println(cmds)
+        for _, host := range HostList {
+                node, err := goeapi.ConnectTo(host)
+                if err != nil {
+                        fmt.Println(err)
+                }
+                fmt.Printf("Connected to %v\n", host)
+                fmt.Println(node.Enable(cmds))
+                fmt.Printf("\n\n-------------------------------\n\n")
+        }
 }
