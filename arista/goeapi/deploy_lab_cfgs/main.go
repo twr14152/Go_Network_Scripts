06fd0200b419e3bf14f99c60ea14493562bc8b2c
Todd Riemenschneider <twr14152@gmail.com>
2024-04-28 10:16:00 -0400

    Update main.go

--- arista/goeapi/deploy_lab_cfgs/main.go
+++ arista/goeapi/deploy_lab_cfgs/main.go
@@ -1,4 +1,3 @@
-// These are partial configs for lab build out
 package main
 
 import (
@@ -17,19 +16,25 @@ func deploy_ceos1() {
         cmds2 := []string{"show ip interface brief | json", "show ip ospf neighbor | json", "show ip bgp summary | json"}
 
         // Configuration commands
-        c1 := "interface ethernet 2"
+        c1 := "interface eth1"
         c2 := "no switchport"
-        c3 := "ip address 10.0.1.1/24"
+        c3 := "ip address 10.0.0.1/24"
         c4 := "no shutdown"
         c5 := "router ospf 1"
-        c6 := "network 10.0.1.0/24 area 0"
+        c6 := "network 10.0.0.0/24 area 0"
         c7 := "router bgp 100"
-        c8 := "neighbor 6.6.6.6 remote-as 100"
-        c9 := "neighbor 6.6.6.6 update-source loopback0"
-        //c10 := ""
-        //c11 := ""
-        //c12 := ""
-        //c13 := ""
+        c8 := "neighbor 2.2.2.2 remote-as 100"
+        c9 := "neighbor 2.2.2.2 update-source loopback0"
+        c10 := "router-id 1.1.1.1"
+        c11 := "network 1.1.1.1 mask 255.255.255.255"
+        c12 := "interface eth3"
+        c13 := "no switchport"
+        c14 := "no shutdown"
+        c15 := "ip address 157.130.1.1/30"
+        c16 := "router bgp 100"
+        c17 := "neighbor 157.130.2.2 remote-as 400"
+        c18 := "redistribute connected"
+
 
 
         node, err := goeapi.ConnectTo(host)
@@ -39,7 +44,7 @@ func deploy_ceos1() {
         fmt.Printf("Connected to %v\n", host)
         fmt.Printf("\n\n\nPre-Change state:\n\n\n")
         fmt.Println(node.Enable(cmds1))
-        fmt.Println(node.ConfigWithErr(c1, c2, c3, c4, c5, c6, c7, c8, c9))
+        fmt.Println(node.ConfigWithErr(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18))
         fmt.Printf("\n\n\nPost-change state: \n\n\n")
         duration := time.Duration(15)*time.Second
         time.Sleep(duration)
@@ -56,19 +61,61 @@ func deploy_ceos2() {
         cmds2 := []string{"show ip interface brief | json", "show ip ospf neighbor | json", "show ip bgp summary | json"}
 
         // Configuration commands
-        c1 := "interface ethernet 2"
+        c1 := "interface ethernet 1"
         c2 := "no switchport"
-        c3 := "ip address 10.0.2.2/24"
+        c3 := "ip address 10.0.0.2/24"
         c4 := "no shutdown"
         c5 := "router ospf 1"
-        c6 := "network 10.0.2.0/24 area 0"
+        c6 := "network 10.0.0.0/24 area 0"
         c7 := "router bgp 100"
-        c8 := "neighbor 6.6.6.6 remote-as 100"
-        c9 := "neighbor 6.6.6.6 update-source loopback 0"
-        //c10 := ""
-        //c11 := ""
-        //c12 := ""
-        //c13 := ""
+        c8 := "neighbor 1.1.1.1 remote-as 100"
+        c9 := "neighbor 1.1.1.1  update-source loopback 0"
+        c10 := "router-id 2.2.2.2"
+        c11 := "network 2.2.2.2 mask 255.255.255.255"
+        c12 := "interface eth3"
+        c13 := "no switchport"
+        c14 := "no shutdown"
+        c15 := "ip address 157.130.2.1/30"
+        c16 := "router bgp 100"
+        c17 := "neighbor 157.130.2.2 remote-as 400"
+        c18 := "redistribute connected"
+
+
+        node, err := goeapi.ConnectTo(host)
+        if err != nil {
+                log.Fatal(err)
+        }
+        fmt.Printf("Connected to %v\n", host)
+        fmt.Printf("\n\n\nPre-Change state:\n\n\n")
+        fmt.Println(node.Enable(cmds1))
+        fmt.Println(node.ConfigWithErr(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18))
+        fmt.Printf("\n\n\nPost-change state: \n\n\n")
+        duration := time.Duration(15)*time.Second
+        time.Sleep(duration)
+        fmt.Println(node.Enable(cmds1))
+        fmt.Println(node.Enable(cmds2))
+        fmt.Printf("\n\n\n\n")
+
+}
+
+func deploy_ceos3() {
+
+        host := "ceos3"
+
+        cmds1 := []string{"show running-config"}
+        cmds2 := []string{"show ip interface brief | json", "show ip ospf neighbor | json", "show ip bgp summary | json"}
+
+        // Configuration commands
+        c1 := "interface ethernet 1"
+        c2 := "no switchport"
+        c3 := "ip address 157.130.1.2/30"
+        c4 := "no shutdown"
+        c5 := "router bgp 300"
+        c6 := "network 3.3.3.3 mask 255.255.255.255"
+        c7 := "router-id 3.3.3.3"
+        c8 := "neighbor 157.130.1.1 remote-as 100"
+        c9 := "redistribute connected"
+
 
 
         node, err := goeapi.ConnectTo(host)
@@ -85,12 +132,13 @@ func deploy_ceos2() {
         fmt.Println(node.Enable(cmds1))
         fmt.Println(node.Enable(cmds2))
         fmt.Printf("\n\n\n\n")
-
 }
 
-func deploy_ceos6() {
 
-        host := "ceos6"
+
+func deploy_ceos4() {
+
+        host := "ceos4"
 
         cmds1 := []string{"show running-config"}
         cmds2 := []string{"show ip interface brief | json", "show ip ospf neighbor | json", "show ip bgp summary | json"}
@@ -98,23 +146,13 @@ func deploy_ceos6() {
         // Configuration commands
         c1 := "interface ethernet 1"
         c2 := "no switchport"
-        c3 := "ip address 10.0.1.6/24"
+        c3 := "ip address 157.130.2.2/30"
         c4 := "no shutdown"
-        c5 := "router ospf 1"
-        c6 := "network 10.0.1.0/24 area 0"
-        c7 := "network 10.0.2.0/24 area 0"
-        c8 := "router bgp 100"
-        c9 := "router-id 6.6.6.6"
-        c10 := "network 6.6.6.6 mask 255.255.255.255"
-        c11 := "redistribute connected"
-        c12 := "neighbor 1.1.1.1 remote-as 100"
-        c13 := "neighbor 1.1.1.1 update-source loopback0"
-        c14 := "neighbor 2.2.2.2 remote-as 100"
-        c15 := "neighbor 2.2.2.2 update-source loopback0"
-        c16 := "interface ethernet 2"
-        c17 := "no switchport"
-        c18 := "ip address 10.0.2.6/24"
-        c19 := "no shutdown"
+        c5 := "router bgp 400"
+        c6 := "network 4.4.4.4 mask 255.255.255.255"
+        c7 := "router-id 4.4.4.4"
+        c8 := "neighbor 157.130.2.1 remote-as 100"
+        c9 := "redistribute connected"
 
 
 
@@ -125,7 +163,7 @@ func deploy_ceos6() {
         fmt.Printf("Connected to %v\n", host)
         fmt.Printf("\n\n\nPre-Change state:\n\n\n")
         fmt.Println(node.Enable(cmds1))
-        fmt.Println(node.ConfigWithErr(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19))
+        fmt.Println(node.ConfigWithErr(c1, c2, c3, c4, c5, c6, c7, c8, c9))
         fmt.Printf("\n\n\nPost-change state: \n\n\n")
         duration := time.Duration(15)*time.Second
         time.Sleep(duration)
@@ -134,10 +172,10 @@ func deploy_ceos6() {
         fmt.Printf("\n\n\n\n")
 }
 
+
 func main() {
         deploy_ceos1()
         deploy_ceos2()
-        deploy_ceos6()
+        deploy_ceos3()
+        deploy_ceos4()
 }
-
-
